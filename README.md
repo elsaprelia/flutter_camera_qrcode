49434cab7caaa320a8b038d31512d8b329fc19ac
elsaprelia <elsaaprelianisutrisno@gmail.com>
2022-09-19 17:25:21 +0700

    pert 30

---
+++ README.md
@@ -0,0 +1,54 @@
+# #30 | Camera dan QR Codes
+
+## Tujuan Praktikum
+
+* Mampu menerapkan fungsi kamera pada aplikasi Flutter
+* Mampu menerapkan plugin QR Codes
+
+## Persiapan
+
+Plugin
+
+* camera
+* path_provider
+* path
+* qr_code_scanner
+
+android/app/buid.gradle 
+
+* compileSdkVersion 32
+* minSdkVersion 21
+
+Aplikasi terdiri dari 3 file meliputi:
+
+* main.dart
+* camera.dart
+* qrcode.dart 
+
+## Hasil Praktikum
+
+![Hasil Praktikum](./images/01.png)
+
+* Halaman Home, terdapat 2 button, yaitu QR Code dan Camera
+
+![Hasil Praktikum](./images/02.png)
+
+* Halaman QR Code, terdapat button untuk scan QR Code
+
+![Hasil Praktikum](./images/03.png)
+
+* Halaman QR Code, terdapat beberapa button
+    * Flash: untuk menyalakan lampu flash
+    * Camera facing back: untuk mengganti kamera belakang
+    * Camera facing front: untuk mengganti kamera depan
+    * Pause: untuk menghentikan scan QR Code
+    * Resume: untuk melanjutkan scan QR Code
+
+![Hasil Praktikum](./images/04.png)
+
+![Hasil Praktikum](./images/05.png)
+
+* Halaman Camera, terdapat button untuk take foto/gambar
+
+
+
---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'camera.dart';
+import 'qrcode.dart';
+import 'dart:async';
+import 'package:camera/camera.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  late final CameraDescription firstCamera;
+
+  Future<void> camera() async {
+    // Ensure that plugin services are initialized so that `availableCameras()`
+    // can be called before `runApp()`
+    WidgetsFlutterBinding.ensureInitialized();
+
+    // Obtain a list of the available cameras on the device.
+    final cameras = await availableCameras();
+
+    // Get a specific camera from the list of available cameras.
+    firstCamera = cameras.first;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Camera dan QR Code')),
+      body: Center(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => const QRCode(),
+                  ));
+                },
+                child: const Text('QR Code'),
+              ),
+            ),
+            Expanded(
+              child: ElevatedButton(
+                onPressed: () {
+                  camera();
+                  Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => TakePictureScreen(
+                      camera: this.firstCamera,
+                    ),
+                  ));
+                },
+                child: const Text('Camera'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
